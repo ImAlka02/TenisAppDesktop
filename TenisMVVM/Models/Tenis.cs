a520b82d7f92a7157d0b9b0f20b56fad5b8e268e
ImAlka02 <ajiju.2000@hotmail.com>
2022-04-29 20:27:21 -0500

    Agregar archivos de proyecto.

---
+++ TenisMVVM/Models/Tenis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenisMVVM.Models
+{
+    public class Tenis
+    {
+        public string Nombre { get; set; }
+        public decimal Valuado { get; set; }
+        public string Historia { get; set; }
+        public string Foto { get; set; }
+
+        public Tenis()
+        {
+            Nombre = "";
+            Historia = "";
+            Foto = "";
+        }
+    }
+}
---
+++ TenisMVVM/ViewModels/TenisViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TenisMVVM.Models;
+
+namespace TenisMVVM.ViewModels
+{
+    public class TenisViewModel
+    {
+        public ObservableCollection <Tenis>
+    }
+}
